93c70d6d541622d70eb31fff36f3c20fb94b0fb7
Leonardo Deganello de Souza <ra98995@uem.br>
2023-09-17 22:59:20 -0300

    better message for ConstraintViolationException

--- src/main/java/com/uem/contas/exceeption/GlobalExceptionHandler.java
+++ src/main/java/com/uem/contas/exceeption/GlobalExceptionHandler.java
@@ -2,13 +2,10 @@ package com.uem.contas.exceeption;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
@@ -19,10 +16,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ConstraintViolationException.class})
     public ProblemDetail handleConstraintViolation(ConstraintViolationException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
         List<String> errors = new ArrayList<>();
         for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
-            errors.add(violation.getRootBeanClass().getName() + " " + violation.getPropertyPath() + ": "
+            errors.add(violation.getPropertyPath() + ": "
                     + violation.getMessage());
         }
         problemDetail.setProperty("errors", errors);
