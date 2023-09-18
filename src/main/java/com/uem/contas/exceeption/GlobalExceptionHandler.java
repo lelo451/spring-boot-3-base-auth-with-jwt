8fe46fc8e714d7134636bd3769ad7703b7fa44b9
Leonardo Deganello de Souza <ra98995@uem.br>
2023-09-18 20:20:59 -0300

    remove unused import

--- src/main/java/com/uem/contas/exceeption/GlobalExceptionHandler.java
+++ src/main/java/com/uem/contas/exceeption/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
